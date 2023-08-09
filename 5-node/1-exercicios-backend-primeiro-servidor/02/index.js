58c1eb8c7bf2ce51514b223d022c4d57526b146a
Patrick Cardoso <dev.patrickcardoso@gmail.com>
2023-08-09 00:40:04 -0300

    first commit

--- 5-node/1-exercicios-backend-primeiro-servidor/02/index.js
+++ 5-node/1-exercicios-backend-primeiro-servidor/02/index.js
@@ -3,40 +3,66 @@ const app = express();
 
 let seconds = 0;
 let minutes = 0;
+let interval;
 
-const formatTimer = () => {
-    const formatedSeconds = String(seconds).padStart(2, '0');
-    const formatedMinutes = String(minutes).padStart(2, '0');
-    const formatedTimer = `${formatedMinutes} minutos e ${formatedSeconds} segundos`;
-    return formatedTimer;
-}
-
-setInterval(() => {
+const timer = () => {
+    if (minutes === 60) {
+        return
+    }   
+    
     seconds++;
 
     if (seconds === 60) {
         seconds = 0;
         minutes++;
     }
-}, 1000);
+};
 
+const play = () => {
+    interval = setInterval(timer, 1000);
+    return interval;
+}
 
-/*
+const pause = (interval) => {
+    clearInterval(interval);
+}
+
+const stop = (interval) => {
+    seconds = 0;
+    minutes = 0;
+    clearInterval(interval);
+}
+
+const formatTimer = () => {
+
+    const formatedSeconds = String(seconds).padStart(2, '0');
+    const formatedMinutes = String(minutes).padStart(2, '0');
+    const formatedTimer = `${formatedMinutes} minutos e ${formatedSeconds} segundos`;
+    return formatedTimer;
+}
+
 app.get('/', (req, res) => {
-    res.send(chronometer);
+    res.send(`Tempo atual do cronometro: ${formatTimer()}`);
 });
 
 app.get('/iniciar', (req, res) => {
+    play();
+    res.send("Cronômetro iniciado!");
 });
 
 app.get('/pausar', (req, res) => {
+    pause(interval);
+    res.send("Cronômetro pausado!")
 });
 
 app.get('/continuar', (req, res) => {
+    play();
+    res.send("Cronômetro continuando!");
 });
 
 app.get('/zerar', (req, res) => {
+    stop(interval);
+    res.send("Cronômetro zerado!");
 });
-*/
 
 app.listen(8000)
