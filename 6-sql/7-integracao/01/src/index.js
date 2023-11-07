b1ca4cbe9f0e5e1d0d4bce25ca5a3b4ac9b732e7
Patrick Cardoso <dev.patrickcardoso@gmail.com>
2023-11-07 00:28:55 -0300

    first commit

---
+++ 6-sql/7-integracao/01/src/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import axios from 'axios';
+import fs from 'fs/promises';
+
+const app = express();
+
+const instanciaAxios = axios.create({
+	baseURL: 'https://companyenrichment.abstractapi.com/v1',
+	params: {
+		api_key: '7858860e5eac4c54a9956b22a9ed0add',
+	},
+})
+
+app.use(express.json());
+
+rotas.get('/empresas', async (req, res) => {
+    const { dominioEmpresa } = req.query;
+
+	try {
+		const { data: empresa } = await instanciaAxios.get(
+			`/?domain=${dominioEmpresa}`
+		);
+
+		if (empresa && empresa.name) {
+			const dadosEmpresas = JSON.parse(
+				await fs.readFile('./src/dados/empresas.json')
+			)
+
+			dadosEmpresas.push(empresa)
+
+			await fs.writeFile(
+				'./src/dados/empresas.json',
+				JSON.stringify(dadosEmpresas, null, 2)
+			)
+		};
+
+		return res.json(empresa);
+	} catch (error) {
+		console.log(error)
+		return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+	}
+});
+
+app.listen(3000);
